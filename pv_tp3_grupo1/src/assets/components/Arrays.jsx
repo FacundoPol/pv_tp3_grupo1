00765175a0916f7493e47509d7635d390448c3c7
Miguel Angel Abregu <miguel_af_555@hotmail.com>
2025-04-29 19:28:41 -0300

    ejercicio 4 funcion ordenar del arreglo de productos - Miguel Abregu

--- pv_tp3_grupo1/src/assets/components/Arrays.jsx
+++ pv_tp3_grupo1/src/assets/components/Arrays.jsx
@@ -1,1 +1,9 @@
 
+const ordenar = (productos) => {    
+  productos.sort((a,b) => a.precio - b.precio);
+  productos.forEach((productos) => {
+    console.log(`Producto [${productos.descripcion}], Precio: $[${productos.precio}]`);
+  });
+  };
+  
+  export default ordenar;
