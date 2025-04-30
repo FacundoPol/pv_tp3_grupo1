0dd53d9e849489ba495bce81ad25f3d1372ae4a8
Gerardo <leon.capo2000@gmail.com>
2025-04-29 23:06:29 -0300

    ejercicio 5 fucion agregar un producto nuevo al final del array -AramayoGerardo

--- pv_tp3_grupo1/src/assets/components/Arrays.jsx
+++ pv_tp3_grupo1/src/assets/components/Arrays.jsx
@@ -1,1 +1,7 @@
 
+const agregarProducto = (productos, nuevoProducto) => {
+productos.push(nuevoProducto) //agrega un producto nuevo al final del array
+console.log("producto agregado:",nuevoProducto)
+
+return productos
+}
